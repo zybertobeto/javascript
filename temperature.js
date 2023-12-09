c82735a30722849d05e47661ec1095fb32fe436e
Albert O <143290938+zybertobeto@users.noreply.github.com>
2023-12-09 23:45:13 +0000

    Modified html, css and js files

--- temperature.js
+++ temperature.js
@@ -1,91 +1,63 @@
 "use strict";
 
-/* HTML Form converting degree Celsius to degree Fahrenheit */
+/* First form converting temperatures */
 
-document.getElementById("calc").onclick = function (tempConversion) {};
-
-/* End of HTML Form temperature conversion */
+document.getElementById("calc").onclick = function (CalcTemp) {
+  const celfahone = document.getElementById("displayarea").value;
+  alert(`The temperature value you entered is ${celfahone}`);
+};
 
 /* Converting degree Celsius to degree Fahrenheit */
 
 document.getElementById("btnCelFah").onclick = function (CelsiusFah) {
-  const celfah = prompt("Please enter Degree Celsius value");
-
-  const celfahresult = (celfah * 9) / 5 + 32;
-  console.log(
-    `${celfah}`,
-    "degrees celsius is equivalent to",
-    `${celfahresult}`,
-    "degrees fahrenheit"
+  const celfah = document.getElementById("tempdisplaybox").value;
+  const celfahresult = Number((celfah * 9) / 5 + 32).toFixed(0);
+  alert(
+    `${celfah} degrees Celsius is equivalent to ${celfahresult}
+    degrees Fahrenheit`
   );
 };
 
 /* Converting degree Fahrenheit to degree Celsius */
 
 document.getElementById("btnFahCel").onclick = function (FahrenheitCel) {
-  const fahcel = prompt("Please enter Degree Fahrenheit value");
-
-  const fahcelresult = ((fahcel - 32) * 5) / 9;
-  console.log(
-    `${fahcel}`,
-    "degrees Fahrenheit is equivalent to",
-    `${fahcelresult}`,
-    "degrees celsius"
+  const fahcel = document.getElementById("tempdisplaybox").value;
+  const fahcelresult = Number(((fahcel - 32) * 5) / 9).toFixed(0);
+  alert(
+    `${fahcel} degrees Fahrenheit is equivalent to ${fahcelresult}
+    degrees celsius`
   );
 };
 
 /* Converting degree Kelvin to degree Fahrenheit */
 
-document.getElementById("btnKelFah").onclick = function (btnKelvinFah) {
-  const kelfah = prompt("Please enter Degree Kelvin value");
-
-  const kelfahresult = ((kelfah - 273.15) * 9) / 5 + 32;
-  console.log(
-    `${kelfah}`,
-    "degrees Kelvin is equivalent to",
-    `${kelfahresult}`,
-    "degrees fahrenheit"
-  );
-};
-
-/* Converting degree Celsius to degree Kelvin */
-
-document.getElementById("btnCelKel").onclick = function (btnCelsiusKel) {
-  const celkel = prompt("Please enter Degree Celsius value");
-
-  const celkelresult = +celkel + +273.15;
-  console.log(
-    `${celkel}`,
-    "degrees celsius is equivalent to",
-    `${celkelresult}`,
-    "degrees kelvin"
+document.getElementById("btnKelFah").onclick = function (KelvinFah) {
+  const kelfah = document.getElementById("tempdisplaybox").value;
+  const kelfahresult = Number((kelfah - 273.15) * (9 / 5) + 32).toFixed(0);
+  alert(
+    `${kelfah} degrees Kelvin is equivalent to ${kelfahresult}
+    degrees Fahrenheit`
   );
 };
 
 /* Converting degree Fahrenheit to degree Kelvin */
 
-document.getElementById("btnFahKel").onclick = function (btnFahrenheitkel) {
-  const fahkel = prompt("Please enter Degree Fahrenheit value");
-
-  const fahkelresult = ((fahkel - 32) * 5) / 9 + 273.15;
-  console.log(
-    `${fahkel}`,
-    "degrees Fahrenheit is equivalent to",
-    `${fahkelresult}`,
-    "degrees kelvin"
+document.getElementById("btnFahKel").onclick = function (FahrenheitKel) {
+  const fahkel = document.getElementById("tempdisplaybox").value;
+  const fahkelresult = Number(((fahkel - 32) * 5) / 9 + 273.15).toFixed(2);
+  alert(
+    `${fahkel} degrees Fahrenheit is equivalent to ${fahkelresult}
+    degrees Kelvin`
   );
 };
 
 /* Converting degree Kelvin to degree Celsius */
 
-document.getElementById("btnKelCel").onclick = function (btnKelvinCel) {
-  const kelcel = prompt("Please enter Degree Kelvin value");
-
-  const kelcelresult = kelcel - 273.15;
-  console.log(
-    `${kelcel}`,
-    "degrees Kelvin is equivalent to",
-    `${kelcelresult}`,
-    "degrees celsius"
+document.getElementById("btnKelCel").onclick = function (KelvinCel) {
+  const kelcel = document.getElementById("tempdisplaybox").value;
+  const kelcelresult = Number(kelcel - 273.15).toFixed(0);
+  alert(
+    `${kelcel} degrees Kelvin is equivalent to ${kelcelresult}
+    degrees Celsius`
   );
 };
