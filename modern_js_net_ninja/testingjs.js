98a2f81d47fa5edc996d15f755f5e52b20d4a17a
Albert Ofori <143290938+zybertobeto@users.noreply.github.com>
2024-12-02 01:57:39 +0000

    Added Net Ninja's Modern Javascript

---
+++ modern_js_net_ninja/testingjs.js
@@ -0,0 +1,26 @@
+// alert("I just run my first JavaScript code.");
+
+// for (let i = 0; i < 400; i += 25) {
+//   console.log(i);
+// }
+
+// const names = ["Jovic", "Ricebottom", "Hinshelwood", "Tinkerer"];
+
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+const names = [
+  "Stephen",
+  "Uranus",
+  "Fishtown",
+  "Hollowbarrel",
+  "Quixote",
+  "Josh",
+];
+
+let i = 0;
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
